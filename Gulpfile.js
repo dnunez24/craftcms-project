caf1f8e43bc2649774699b1dd310f52081cec5ab
Dave Nuñez <dnunez24@gmail.com>
2017-02-22 23:27:06 -0800

    Add skeleton for build tools

---
+++ Gulpfile.js
@@ -0,0 +1,14 @@
+// autoprefixer
+// uglify
+// eslint
+// stylelint
+// sass
+// imagemin
+// browsersync
+
+var config = {
+  assets: {
+    src: 'src/assets',
+    dest: 'src/public/assets'
+  }
+};
