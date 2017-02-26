e7838774e0957e69edcad885895e2045552f34fe
Dave Nuñez <dnunez24@gmail.com>
2017-02-26 11:32:32 -0800

    Replace tabs with spaces for redis config

--- src/config/rediscache.php
+++ src/config/rediscache.php
@@ -2,10 +2,10 @@
 
 return array(
     '*' => array(
-    	'hostname' => getenv('CACHE_HOST') ?: 'localhost',
-    	'port'     => getenv('CACHE_PORT') ?: '6379',
-    	'password' => getenv('CACHE_PASS') ?: '',
-    	'database' => 0,
-    	'timeout'  => null,
+        'hostname' => getenv('CACHE_HOST') ?: 'localhost',
+        'port'     => getenv('CACHE_PORT') ?: '6379',
+        'password' => getenv('CACHE_PASS') ?: '',
+        'database' => 0,
+        'timeout'  => null,
     ),
 );
