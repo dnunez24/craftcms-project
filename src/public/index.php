65365c7dc87f59326f66cc9da1166efd7c4faed7
Dave Nuñez <dnunez24@gmail.com>
2017-02-26 11:32:19 -0800

    Fix syntax for index.php

--- src/public/index.php
+++ src/public/index.php
@@ -20,14 +20,12 @@ define('CRAFT_BASE_PATH', $root);
 
 $path = $root.'/vendor/craftcms/cms/app/index.php';
 
-if (!is_file($path))
-{
-	if (function_exists('http_response_code'))
-	{
-		http_response_code(503);
-	}
+if (!is_file($path)) {
+    if (function_exists('http_response_code')) {
+        http_response_code(503);
+    }
 
-	exit('Could not find your craft/ folder. Please ensure that <strong><code>$craftPath</code></strong> is set correctly in '.__FILE__);
+    exit('Could not find your craft/ folder. Please ensure that <strong><code>$craftPath</code></strong> is set correctly in '.__FILE__);
 }
 
 require_once $path;
