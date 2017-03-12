b9ea9005bebe06d6c335ee2e5c76f779e7998b1b
Dave Nuñez <dnunez24@gmail.com>
2017-03-11 22:42:29 -0800

    Add fix for MySQL 5.7.5+

--- src/config/db.php
+++ src/config/db.php
@@ -16,4 +16,5 @@ return [
     'schema'        => getenv('DB_SCHEMA') ?: 'public',
     'tablePrefix'   => getenv('DB_TABLE_PREFIX') ?: '',
     'port'          => getenv('DB_PORT') ?: '3306',
+    'initSQLs' => ["SET SESSION sql_mode='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';"],
 ];
