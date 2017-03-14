c91e5197b240210f973743708514eec52f6e00f7
Dave Nuñez <dnunez24@gmail.com>
2017-03-13 22:04:08 -0700

    Update general config for platform.sh

--- src/config/general.php
+++ src/config/general.php
@@ -1,17 +1,37 @@
 <?php
 
-return [
-    '*' => [
-        'cacheMethod'                   => 'redis',
-        'cpTrigger'                     => 'admin',
-        'enableCsrfProtection'          => true,
-        'omitScriptNameInUrls'          => true,
-        'siteUrl'                       => getenv('BASE_URL') ?: 'http://dev.craftcms.com',
-        'usePathInfo'                   => true,
-        'overridePhpSessionLocation'    => getenv('SESSION_LOCATION'),
-    ],
-    'dev' => [
-        'devMode'                       => true,
-        'useCompressedJs'               => false,
-    ],
+$environment = getenv('CRAFT_ENVIRONMENT');
+$routesEncoded = getenv('PLATFORM_ROUTES');
+$applicationName = getenv('PLATFORM_APPLICATION_NAME');
+$entropy = getenv('PLATFORM_PROJECT_ENTROPY');
+
+$config = [];
+
+$config['*'] = [
+    'allowAutoUpdates'              => false,
+    'appId'                         => 'craftcms',
+    'cacheMethod'                   => 'redis',
+    'cpTrigger'                     => 'admin',
+    'enableCsrfProtection'          => true,
+    'errorTemplatePrefix'           => '_errors/',
+    'omitScriptNameInUrls'          => true,
+    'usePathInfo'                   => true,
+    'overridePhpSessionLocation'    => true,
+    'allowAutoUpdates'              => false,
+    'devMode'                       => $environment === 'dev',
+    'useCompressedJs'               => $environment !== 'dev',
+    'validationKey'                 => $entropy,
 ];
+
+if ($routesEncoded) {
+    $routes = json_decode(base64_decode($routesEncoded), true);
+
+    foreach ($routes as $url => $route) {
+        if ($route['type'] === 'upstream' && $route['upstream'] === $applicationName) {
+            $host = parse_url($url, PHP_URL_HOST);
+            $config[$host] = ['siteUrl' => $url];
+        }
+    }
+}
+
+return $config;
