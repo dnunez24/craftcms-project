d69c7bddfe9a401087810553b44e87db7d464266
Dave Nuñez <dnunez24@gmail.com>
2017-05-01 20:18:39 -0700

    Add cssnano for production assets build

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,12 +1,23 @@
 const { mix } = require('laravel-mix');
-// const autoprefixer = require('autoprefixer');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+const postCssConfig = function postCssConfig() {
+  const config = [
+    autoprefixer(),
+  ];
+
+  if (mix.config.inProduction) {
+    config.push(cssnano());
+  }
+
+  return config;
+};
 
 mix.sass('src/assets/styles/main.scss', 'public/assets/styles')
-.options({
-  // postCss: [
-  //   autoprefixer(),
-  // ],
-});
+  .options({
+    postCss: postCssConfig(),
+  });
 
 mix.js('src/assets/scripts/main.js', 'public/assets/scripts')
   .sourceMaps();
