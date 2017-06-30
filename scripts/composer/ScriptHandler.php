ddb1b4ac4799e447c682c4dcc0442da370a75718
David Nuñez <david.nunez@alumni.stanford.edu>
2017-06-29 21:40:53 -0700

    Clean up composer script handler

--- scripts/composer/ScriptHandler.php
+++ scripts/composer/ScriptHandler.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Dnunez24\composer;
+namespace Copious\Composer;
 
 use Composer\IO\IOInterface;
 use Composer\Script\Event;
@@ -8,120 +8,81 @@ use Composer\Util\Filesystem;
 
 class ScriptHandler
 {
-    const DEFAULT_PROJECT_NAME = 'craftcms';
-
-    public static function createDotEnvFile(Event $event)
-    {
-        $io = $event->getIO();
-        $filename = self::getDotEnvFilename();
-
-        self::confirmOverwriteFile($io, $filename);
-
-        $file = fopen($filename, 'w');
-        $projectName = self::askProjectName($io);
-
-        fwrite($file, "BASE_URL=http://www.".$projectName.".dev\n");
-        fwrite($file, "COMPOSE_PROJECT_NAME=".$projectName."\n");
-        fwrite($file, "CACHE_HOST=cache.".$projectName."_private\n");
-        fwrite($file, "CACHE_PORT=6379\n");
-        fwrite($file, "DB_DRIVER=mysql\n");
-        fwrite($file, "DB_SERVER=db.".$projectName."_private\n");
-        fwrite($file, "DB_DATABASE=".$projectName."\n");
-        fwrite($file, "DB_USER=".$projectName."\n");
-        fwrite($file, "DB_PASSWORD=".$projectName."\n");
-        fwrite($file, "DB_PORT=3306\n");
-        fwrite($file, "SESSION_LOCATION=tcp://session.".$projectName."_private:6379\n");
-        fwrite($file, "TZ=UTC\n");
-        fclose($file);
-    }
-
+    /**
+     * Re-maps Craft plugin files to new location (to handle plugins with
+     * incompatible directory structure)
+     *
+     * @param Event $event Composer script event
+     * @return void
+     */
     public static function remapCraftPlugins(Event $event)
     {
-        $extra = $event->getComposer()->getPackage()->getExtra();
-
-        if (!array_key_exists('craft-plugin-map', $extra)) {
-            return;
-        }
-
-        $pluginMap = $extra['craft-plugin-map'];
-
         $io = $event->getIO();
         $fs = new Filesystem();
-        $pluginsPath = self::getProjectRoot().'/craft/plugins/';
+        $extra = self::getExtraConfig($event);
+        $pluginsPath = self::getPluginsPath($extra);
+        $pluginsMap = self::getPluginsMap($extra);
 
-        foreach ($pluginMap as $src => $dest) {
+        foreach ($pluginsMap as $src => $dest) {
+            $srcParts = explode('/', $src);
+            $srcLvl1 = $srcParts[0];
+            $srcLvl2 = $srcParts[1];
+
+            $srcRoot = $fs->normalizePath($pluginsPath.$srcLvl1);
             $srcDir = $fs->normalizePath($pluginsPath.$src);
             $destDir = $fs->normalizePath($pluginsPath.$dest);
 
             if (is_dir($srcDir)) {
                 $io->write("Moving $srcDir to $destDir");
                 $fs->rename($srcDir, $destDir);
+
+                if ($srcLvl1 !== $srcLvl2) {
+                    $fs->removeDirectory($srcRoot);
+                }
             }
         }
     }
 
     protected static function getProjectRoot()
     {
-        return dirname(dirname(__DIR__));
+        return dirname(dirname(__DIR__)).'/';
     }
 
-    protected static function getProjectPath($path)
+    protected static function getProjectPath(string $path)
     {
-        return realpath(self::getProjectRoot().$path);
+        return realpath(self::getProjectRoot().'/'.$path);
     }
 
-    protected static function getPluginPath($path)
+    protected static function getExtraConfig(Event $event)
     {
-        $base = self::getProjectPath('craft/plugins');
+        return $event->getComposer()->getPackage()->getExtra();
+    }
 
-        if ($fs->isAbsolutePath($path)) {
-            return $path;
+    protected static function getPluginsPath(array $extra)
+    {
+        $pluginsPath = $extra['craft-plugins-path'];
+
+        if (!isset($pluginsPath)) {
+            throw new Exception("'extra.craft-plugins-path' composer option must be set.");
         }
 
-        return rtrim($base, '/') . '/' . ltrim($path, '/');
+        return self::getProjectPath($pluginsPath).'/';
     }
 
-    public static function getDotEnvFilename()
+    /**
+     * Gets Craft plugin path mappings from Composer config
+     *
+     * @param array $extra Composer 'extra' config object
+     * @return array Map of plugin paths
+     */
+    protected static function getPluginsMap(array $extra)
     {
-        $projectRoot = dirname(dirname(__DIR__));
-        $dotEnvBasename = '/.env';
-        return $projectRoot.$dotEnvBasename;
-    }
+        $pluginsMap = $extra['craft-plugins-map'];
 
-    protected static function confirmOverwriteFile(IOInterface $io, $filename)
-    {
-        $overwrite = true;
-
-        if (file_exists($filename)) {
-            $question = "A .env file already exists at the project root. Do you want to overwrite it? ";
-            $overwrite = $io->askConfirmation($question);
+        if (!$pluginsMap) {
+            return [];
         }
 
-        $overwrite or exit;
-    }
-
-    protected static function askProjectName(IOInterface $io)
-    {
-        $default = self::DEFAULT_PROJECT_NAME;
-        $question = "What is the project name? (default: {$default}) ";
-        $attempts = 1;
-        $validator = array(get_called_class(), 'validateProjectName');
-        return $io->askAndValidate($question, $validator, $attempts, $default);
-    }
-
-    public static function validateProjectName($projectName)
-    {
-        $projectName = self::makeValidProjectName($projectName);
-
-        if (preg_match('/[^\w]/', $projectName)) {
-            throw new Exception("Project name must match character class [a-zA-Z0-9_]");
-        }
-
-        return $projectName;
-    }
-
-    protected static function makeValidProjectName($name)
-    {
-        return strtolower(preg_replace('/[^\w]/', '_', $name));
+        return $pluginsMap;
     }
 }
